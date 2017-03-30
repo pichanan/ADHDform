24828fd07c4e5c7616b00b0cf3ce01e74a717e2a
pichanan95 <pimpichachacha@gmail.com>
2017-03-30 10:59:52 +0700

    finished

--- app/src/main/java/adhd/sirikan/pimpicha/adhdform/MainActivity.java
+++ app/src/main/java/adhd/sirikan/pimpicha/adhdform/MainActivity.java
@@ -11,10 +11,12 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-        //explicit
+    //explicit
     private EditText userEditText, passwordEditText;
     private TextView textView;
     private Button button;
+    private String userString,passString;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,7 +26,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         initialWidget();
         //controller
         controller();
-
 
 
     }// main method
@@ -38,15 +39,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         userEditText = (EditText) findViewById(R.id.edtUser);
         passwordEditText = (EditText) findViewById(R.id.edtpassword);
         textView = (TextView) findViewById(R.id.txtNewRegister);
-        button  = (Button) findViewById(R.id.btnLogin);
+        button = (Button) findViewById(R.id.btnLogin);
     }
 
     @Override
     public void onClick(View v) {
         //for textView
         if (v == textView) {
-            startActivity(new Intent(MainActivity.this,RegisterActivity.class));
+            startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+        }
+        //for Button
+        if (v == button) {
+            // get value from Edit text
+            userString= userEditText.getText().toString().trim();
+            passString = passwordEditText.getText().toString().trim();
+            // check spece
+            if (userString.equals("")|| passString.equals("")) {
+                //have space
+                myAlert myAlert = new myAlert(MainActivity.this);
+                String s = getResources().getString(R.string.title_havespace);
+                String s1 = getResources().getString(R.string.message_havespece);
+                myAlert.myDialog(s,s1);
+            } else {
+                //no space
+            }
         }
 
-    }
+
+
+    }//onclick
 }//main class
---
+++ app/src/main/java/adhd/sirikan/pimpicha/adhdform/myAlert.java
@@ -0,0 +1,32 @@
+package adhd.sirikan.pimpicha.adhdform;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+/**
+ * Created by TheUser on 30/3/2560.
+ */
+
+public class myAlert {
+    private Context context;
+
+    public myAlert(Context context) {
+        this.context = context;
+    }
+
+    public void myDialog(String strTitle,String strMassage) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setCancelable(false);
+        builder.setIcon(R.mipmap.ic_user);
+        builder.setTitle(strTitle);
+        builder.setMessage(strMassage);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+    }
+}//main
